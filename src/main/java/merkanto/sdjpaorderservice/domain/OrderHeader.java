746d6909d84fceacc458a52d050c894bacd605a7
Merkanto <miroslav.i.zhelezchev@gmail.com>
2022-08-13 17:37:26 +0300

    Adding Embedded types.

--- src/main/java/merkanto/sdjpaorderservice/domain/OrderHeader.java
+++ src/main/java/merkanto/sdjpaorderservice/domain/OrderHeader.java
@@ -1,11 +1,55 @@
 package merkanto.sdjpaorderservice.domain;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+
+import java.util.Objects;
 
 @Entity
+@AttributeOverrides({
+        @AttributeOverride(
+                name = "shippingAddress.address",
+                column = @Column(name = "shipping_address")
+        ),
+        @AttributeOverride(
+                name = "shippingAddress.city",
+                column = @Column(name = "shipping_city")
+        ),
+        @AttributeOverride(
+                name = "shippingAddress.state",
+                column = @Column(name = "shipping_state")
+        ),
+        @AttributeOverride(
+                name = "shippingAddress.zipCode",
+                column = @Column(name = "shipping_zip_code")
+        ),
+        @AttributeOverride(
+                name = "billToAddress.address",
+                column = @Column(name = "bill_to_address")
+        ),
+        @AttributeOverride(
+                name = "billToAddress.city",
+                column = @Column(name = "bill_to_city")
+        ),
+        @AttributeOverride(
+                name = "billToAddress.state",
+                column = @Column(name = "bill_to_state")
+        ),
+        @AttributeOverride(
+                name = "billToAddress.zipCode",
+                column = @Column(name = "bill_to_zip_code")
+        )
+})
 public class OrderHeader extends BaseEntity {
 
     private String customer;
+
+
+    @Embedded
+    private Address shippingAddress;
+
+
+    @Embedded
+    private Address billToAddress;
 
     public String getCustomer() {
         return customer;
@@ -15,21 +59,42 @@ public class OrderHeader extends BaseEntity {
         this.customer = customer;
     }
 
+    public Address getShippingAddress() {
+        return shippingAddress;
+    }
+
+    public void setShippingAddress(Address shippingAddress) {
+        this.shippingAddress = shippingAddress;
+    }
+
+    public Address getBillToAddress() {
+        return billToAddress;
+    }
+
+    public void setBillToAddress(Address billToAddress) {
+        this.billToAddress = billToAddress;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof OrderHeader)) return false;
+        if (o == null || getClass() != o.getClass()) return false;
         if (!super.equals(o)) return false;
 
         OrderHeader that = (OrderHeader) o;
 
-        return getCustomer() != null ? getCustomer().equals(that.getCustomer()) : that.getCustomer() == null;
+        if (!Objects.equals(customer, that.customer)) return false;
+        if (!Objects.equals(shippingAddress, that.shippingAddress))
+            return false;
+        return Objects.equals(billToAddress, that.billToAddress);
     }
 
     @Override
     public int hashCode() {
         int result = super.hashCode();
-        result = 31 * result + (getCustomer() != null ? getCustomer().hashCode() : 0);
+        result = 31 * result + (customer != null ? customer.hashCode() : 0);
+        result = 31 * result + (shippingAddress != null ? shippingAddress.hashCode() : 0);
+        result = 31 * result + (billToAddress != null ? billToAddress.hashCode() : 0);
         return result;
     }
 }
