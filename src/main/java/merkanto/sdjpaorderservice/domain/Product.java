34a4e8f791d52c77eff89be757075264b85f3bcf
Merkanto <miroslav.i.zhelezchev@gmail.com>
2022-08-15 16:02:22 +0300

    Adding many to many relationship.

--- src/main/java/merkanto/sdjpaorderservice/domain/Product.java
+++ src/main/java/merkanto/sdjpaorderservice/domain/Product.java
@@ -1,10 +1,9 @@
 package merkanto.sdjpaorderservice.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
+import jakarta.persistence.*;
 
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 public class Product extends BaseEntity {
@@ -13,6 +12,12 @@ public class Product extends BaseEntity {
 
     @Enumerated(EnumType.STRING)
     private ProductStatus productStatus;
+
+    @ManyToMany
+    @JoinTable(name = "product_category",
+        joinColumns = @JoinColumn (name = "product_id"),
+        inverseJoinColumns = @JoinColumn (name = "category_id"))
+    private Set<Category> categories;
 
     public String getDescription() {
         return description;
@@ -30,23 +35,13 @@ public class Product extends BaseEntity {
         this.productStatus = productStatus;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        if (!super.equals(o)) return false;
-
-        Product product = (Product) o;
-
-        if (!Objects.equals(description, product.description)) return false;
-        return productStatus == product.productStatus;
+    public Set<Category> getCategories() {
+        return categories;
     }
 
-    @Override
-    public int hashCode() {
-        int result = super.hashCode();
-        result = 31 * result + (description != null ? description.hashCode() : 0);
-        result = 31 * result + (productStatus != null ? productStatus.hashCode() : 0);
-        return result;
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
+
+
 }
