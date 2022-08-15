39c865237d2b663cba69cb469526eec24239d799
Merkanto <miroslav.i.zhelezchev@gmail.com>
2022-08-15 15:24:25 +0300

    Adding many to one unidirectional.

--- src/test/java/merkanto/sdjpaorderservice/repositories/OrderHeaderRepositoryTest.java
+++ src/test/java/merkanto/sdjpaorderservice/repositories/OrderHeaderRepositoryTest.java
@@ -2,6 +2,9 @@ package merkanto.sdjpaorderservice.repositories;
 
 import merkanto.sdjpaorderservice.domain.OrderHeader;
 import merkanto.sdjpaorderservice.domain.OrderLine;
+import merkanto.sdjpaorderservice.domain.Product;
+import merkanto.sdjpaorderservice.domain.ProductStatus;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -21,6 +24,19 @@ class OrderHeaderRepositoryTest {
     @Autowired
     OrderHeaderRepository orderHeaderRepository;
 
+    @Autowired
+    ProductRepository productRepository;
+
+    Product product;
+
+    @BeforeEach
+    void setUp() {
+        Product newProduct = new Product();
+        newProduct.setProductStatus(ProductStatus.NEW);
+        newProduct.setDescription("test product");
+        product = productRepository.saveAndFlush(newProduct);
+    }
+
     @Test
     void testSaveOrderWithLine () {
         OrderHeader orderHeader = new OrderHeader();
@@ -29,6 +45,7 @@ class OrderHeaderRepositoryTest {
 
         OrderLine orderLine = new OrderLine();
         orderLine.setQuantityOrdered(5);
+        orderLine.setProduct(product);
 
         orderHeader.setOrderLines(Set.of(orderLine));
         orderLine.setOrderHeader(orderHeader);
