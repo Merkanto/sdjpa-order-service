f63bed85796e57efe873f265fb1a1ebff83f7aaa
Merkanto <miroslav.i.zhelezchev@gmail.com>
2022-08-15 22:17:24 +0300

    Adding Hibernate N + 1 Problem analysis and test.

--- src/main/java/merkanto/sdjpaorderservice/repositories/OrderHeaderRepository.java
+++ src/main/java/merkanto/sdjpaorderservice/repositories/OrderHeaderRepository.java
@@ -1,7 +1,13 @@
 package merkanto.sdjpaorderservice.repositories;
 
+import merkanto.sdjpaorderservice.domain.Customer;
 import merkanto.sdjpaorderservice.domain.OrderHeader;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
+import java.util.List;
+
 public interface OrderHeaderRepository extends JpaRepository<OrderHeader, Long> {
+
+    List<OrderHeader> findAllByCustomer(Customer customer);
 }
--- src/test/java/merkanto/sdjpaorderservice/DataLoadTest.java
+++ src/test/java/merkanto/sdjpaorderservice/DataLoadTest.java
@@ -13,8 +13,10 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.ArrayList;
+import java.util.IntSummaryStatistics;
 import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 @ActiveProfiles("local")
 @DataJpaTest
@@ -36,6 +38,18 @@ public class DataLoadTest {
     ProductRepository productRepository;
 
     @Test
+    void testN_PlusOneProblem() {
+
+        Customer customer = customerRepository.findCustomerByCustomerNameIgnoreCase(TEST_CUSTOMER).get();
+
+        IntSummaryStatistics totalOrdered = orderHeaderRepository.findAllByCustomer(customer).stream()
+                .flatMap(orderHeader -> orderHeader.getOrderLines().stream())
+                .collect(Collectors.summarizingInt(ol -> ol.getQuantityOrdered()));
+
+        System.out.println("total ordered: " + totalOrdered.getSum());
+    }
+
+    @Test
     void testLazyVsEager() {
 
         OrderHeader orderHeader = orderHeaderRepository.getById(5l);
@@ -44,14 +58,14 @@ public class DataLoadTest {
 
     }
 
-    @Disabled
+//    @Disabled
     @Rollback(value = false)
     @Test
     void testDataLoader() {
         List<Product> products = loadProducts();
         Customer customer = loadCustomers();
 
-        int ordersToCreate = 100;
+        int ordersToCreate = 10000;
 
         for (int i = 0; i < ordersToCreate; i++){
             System.out.println("Creating order #: " + i);
