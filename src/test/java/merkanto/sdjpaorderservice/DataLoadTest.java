55ddccac5ba8440df34b2276de69e1cc849a6ced
Merkanto <miroslav.i.zhelezchev@gmail.com>
2022-08-16 09:32:49 +0300

    Adding DB locking test.

--- src/test/java/merkanto/sdjpaorderservice/DataLoadTest.java
+++ src/test/java/merkanto/sdjpaorderservice/DataLoadTest.java
@@ -38,6 +38,20 @@ public class DataLoadTest {
     ProductRepository productRepository;
 
     @Test
+    void testDBLock() {
+        Long id = 1l;
+
+        OrderHeader orderHeader = orderHeaderRepository.findById(id).get();
+
+        Address billTo = new Address();
+        billTo.setAddress("Bill me");
+        orderHeader.setBillToAddress(billTo);
+        orderHeaderRepository.saveAndFlush(orderHeader);
+
+        System.out.println("I updated the order");
+    }
+
+    @Test
     void testN_PlusOneProblem() {
 
         Customer customer = customerRepository.findCustomerByCustomerNameIgnoreCase(TEST_CUSTOMER).get();
